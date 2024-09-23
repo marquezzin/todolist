cbec3f8c17637f133214b7e91984c2833b21845e
marquezin <gabrielmarques2003@gmail.comgit config --global user.email gabrielmarques2003@gmail.comgit config --global user.name marquezzinclear>
2024-09-23 16:28:05 -0300

    setting empty box

--- src/componentes/TaskSpace.jsx
+++ src/componentes/TaskSpace.jsx
@@ -1,17 +1,31 @@
 import styles from './TaskSpace.module.css'
+import clipboard from '../assets/Clipboard.svg'
 
 export function TaskSpace(){
     return(
-        <div className={styles.info}>
-            <div className={styles.created}>
-                <p>Tarefas criadas</p>
-                <span>0</span>
-            </div>
-            <div className={styles.completed}>
-                <p>Concluídas</p>
-                <span>0</span>
+        <div>
+            <header className={styles.info}>
+                <div className={styles.created}>
+                    <p>Tarefas criadas</p>
+                    <span>0</span>
+                </div>
+                <div className={styles.completed}>
+                    <p>Concluídas</p>
+                    <span>0</span>
+                </div>
+            </header>
+            <div className={styles.boxEmpty}>
+                {/* if tasks == 0 */}
+                <img src={clipboard} alt="clipboard" />
+                <p className={styles.bold}>Você ainda não tem tarefas cadastradas</p>
+                <p>Crie tarefas e organize seus itens a fazer</p>
+
+
             </div>
         </div>
+
+
+
     )
 
 }
